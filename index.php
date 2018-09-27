472d7e78dc3f382a0264b773e3b0e3683d307701
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-09-27 22:54:23 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+    <!-- Basic Page Needs -->
+        <meta charset="utf-8">
+        <title>BRAINRAIN</title>
+        <meta name="description" content="">
+        <meta name="author" content="Dominik Hillmann">
+
+    <!-- Mobile Specific Metas -->
+        <meta>
+
+    <!-- CSS links -->
+        <link rel="stylesheet" href="./css/main.css">
+        <!-- <link rel="stylesheet" href="css/mobile.css"> -->
+
+    <!-- Favicon -->
+        <link rel="icon" type="image/png" href="favicon.ico">
+    </head>
+    <body>
+        <header>
+            <div>
+                <img src="./img/burgermenu.png">
+                <h1>Pfad mit Links</h1>
+            </div>
+
+            <img src="./img/brainrainlogo.png">
+
+            <h1>You're my number: n</h1>
+        </header>
+        Das ist ein Test.
+    </body>
+</html>
