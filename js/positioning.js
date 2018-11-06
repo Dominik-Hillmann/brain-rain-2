aa62ddc925d29e13a6c00d1a9a64d9f314f1bfc0
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-11-06 23:15:01 +0100

    Add files via upload

--- js/positioning.js
+++ js/positioning.js
@@ -1,18 +1,40 @@
+function pxToFloat(pxStr) {
+    if (pxStr.substring(pxStr.length - 2, pxStr.length) != "px") {
+        throw new Error("pxStr is not a string containing 'px' at the end.");
+    } else {
+        return parseFloat(pxStr.substring(0, pxStr.length - 3));
+    }
+}
+
 function centerLogo() {
-    let menu = document.querySelector("#firstmenu");
-    let logo = menu.querySelectorAll("img")[1];
-    let style = window.getComputedStyle(logo);
-    console.log(style.marginLeft);
-    console.log(style.marginRight);
-    // width des Elternelement
-    // width links und rechts
-    // margins so setzen, dass Gehirn in der Mitte ist
+
+    let logoMarginLeft = pxToFloat(window.getComputedStyle(logo).marginLeft);
+    let leftWidth = pxToFloat(window.getComputedStyle(left).width);
+    let rightWidth = pxToFloat(window.getComputedStyle(right).width);
+
+    // console.log(logoMarginLeft);
+    // console.log(leftWidth);
+
+    console.log(window.getComputedStyle(logo));
+
+    // logo.style.marginLeft = (logoMarginLeft + rightWidth) + "px";
+    // console.log(leftWidth - rightWidth);
+
+
+//     "margin-left": "587.767px"
+// ​
+// "margin-right": "424.933px"
+// marginLeft: "587.767px"
+// ​
+// marginRight: "424.933px"
 }
 
 
-
+let menu = document.querySelector("#firstmenu");
+let logo = menu.querySelectorAll("img")[1];
+let left = menu.querySelector("#path");
+let right = menu.querySelector("#number");
 
 window.onload = function () {
-    console.log("da");
     centerLogo();
 }
