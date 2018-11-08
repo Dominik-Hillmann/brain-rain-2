4eea3854f6793c00e661d60bfafa14fe050e6515
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-11-08 20:00:47 +0100

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,8 +1,18 @@
 function stickyHeader() {
     let header = document.querySelector("header");
     console.log(window.getComputedStyle(header).height);
+
+    if (window.pageYOffset > 5) {
+        heading.classList.add("retract");
+        heading.classList.remove("extend");
+    } else {
+        heading.classList.remove("retract");
+        heading.classList.add("extend");
+    }
 }
 
+let heading = document.querySelector("#heading");
+console.log(heading);
 
 window.onscroll = function () {
     stickyHeader();
