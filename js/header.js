1640a9cf996a0c96ab48fe4f173d6e4d53b2a000
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-11-09 20:57:19 +0100

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,18 +1,17 @@
 function stickyHeader() {
-    let header = document.querySelector("header");
-    console.log(window.getComputedStyle(header).height);
-
-    if (window.pageYOffset > 5) {
+    if (window.pageYOffset > 15) {
         heading.classList.add("retract");
         heading.classList.remove("extend");
+        innerHeader.classList.add("has-bottom-border");
     } else {
         heading.classList.remove("retract");
         heading.classList.add("extend");
+        innerHeader.classList.remove("has-bottom-border");
     }
 }
 
 let heading = document.querySelector("#heading");
-console.log(heading);
+let innerHeader = document.querySelector("#inner-header-wrapper");
 
 window.onscroll = function () {
     stickyHeader();
--- photography.php
+++ photography.php
@@ -26,14 +26,15 @@
 
     <div id="currently-shown" class="hide">
         <!-- here to cover all of the body -->
-        <img src="./img/cross.png" id="cross" onclick="unblurBackground();">
+        <img src="./img/cross_colored.png" id="cross" onclick="unblurBackground();">
         <div id="main-pic-container">
             <img src="./img/cube.gif" id="the-main-pic">
         </div>
         <div id="curr-pic-info-wrapper">
-            <img src="./img/arrow_links.png" class="arrow" onclick="prevPic();"><!--
-            --><img src="./img/arrow_rechts.png" class="arrow" id="rarrow" onclick="nextPic();"><!--
+            <img src="./img/arrow_links.png_colored" class="arrow" onclick="prevPic();"><!--
+            --><img src="./img/arrow_rechts_colored.png" class="arrow" id="rarrow" onclick="nextPic();"><!--
             --><div id="curr-pic-info">
+                <h1>Titel, ein langer Titel</h1>
                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
             </div>
         </div>
@@ -44,28 +45,31 @@
         <?php require "./libraries/util.inc.php"; ?>
         <header>
 
-            <div id="firstmenu" class="menufloater">
-                <div id="path">
-                    <img src="./img/burgermenu.png">
-                    <h1><a href="http://brain-rain.net">Menu</a> / Photography</h1>
-                </div>
-
-                <div id="number" class="menufloater">
-                    <h1>You're my number:</h1>
-                    <div><!--
-                        --><p>1</p><!--
-                        --><p>2</p><!--
-                        --><p>3</p>
+            <div id="inner-header-wrapper">
+                <div id="firstmenu" class="menufloater">
+                    <div id="path">
+                        <img src="./img/burgermenu.png">
+                        <h1><a href="http://brain-rain.net">Menu</a> / Photography</h1>
                     </div>
+
+                    <div id="number" class="menufloater">
+                        <h1>You're my number:</h1>
+                        <div><!--
+                            --><p>1</p><!--
+                            --><p>2</p><!--
+                            --><p>3</p>
+                        </div>
+                    </div>
+
+                    <img src="./img/brainrainlogo.png">
                 </div>
-
-                <img src="./img/brainrainlogo.png">
             </div>
 
-            <div id="heading">
-                <h1>BRAINRAIN</h1>
-                <h2><?php echo num2Roman((int) date("Y")); ?></h2>
-            </div>
+                <div id="heading">
+                    <h1>BRAINRAIN</h1>
+                    <h2><?php echo num2Roman((int) date("Y")); ?></h2>
+                </div>
+            <!--</div>-->
 
         </header>
 
