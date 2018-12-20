ef8ad1e30cff9496a00462783ac708616d0a89f3
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-12-20 22:30:01 +0100

    Add files via upload

--- database.php
+++ database.php
@@ -2,6 +2,12 @@
 /*
     Grundidee:
     verschlüsselte Passwörter außerhalb des Ordners speichern, auf den Nutzer zugreifen können
+    dieser Ordner enthält je
+        das verschlüsselte Passwort
+        eine Liste von
+            Bildern
+            Texten
+            Vorstellungen, die nur auf der Seite angezeigt werden sollen
     Name
     Password
     Liste an Units, die dem Nutzer zugänglich sein sollen
